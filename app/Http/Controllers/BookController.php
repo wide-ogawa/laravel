90655b51c5bc628ee200f09676f9301bee4201ef
wide-ogawa <80499017+wide-ogawa@users.noreply.github.com>
2021-10-15 20:26:04 +0900

    バリデーション完成

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookRequest;
 use App\Models\Book;
 
 class BookController extends Controller
@@ -26,7 +26,7 @@ class BookController extends Controller
         return view('book/edit', compact('book'));
     }
 
-    public function update(Request $request, $id)
+    public function update(BookRequest $request, $id)
     {
         $book = Book::findOrFail($id);
         $book->name = $request->name;
@@ -52,7 +52,7 @@ class BookController extends Controller
         return view('book/create', compact('book'));
     }
 
-    public function store(Request $request)
+    public function store(BookRequest  $request)
     {
         $book = new Book();
         $book->name = $request->name;
